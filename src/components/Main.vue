60fc62a1ab25bc45c16c63c0b65cda8258854cae
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-24 14:41:06 +0300

    replace HelloWorld with Main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<script>
+
+    export default {
+
+        name: 'Main',
+
+    }
+
+</script>
+
+<template>
+
+    <div>
+        TEST
+    </div>
+    <div>Main page</div>
+
+</template>
+
+<style scoped>
+
+</style>
