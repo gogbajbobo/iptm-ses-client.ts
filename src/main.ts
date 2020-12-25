1d6a87f3f39a1a28ba0669cf76fcdee4dac61ff4
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-25 15:57:36 +0300

    app.use(store)

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,30 @@ import App from './App.vue'
 import { elements } from "@/libs/element-ui"
 
 import { router } from './router'
+import { createStore } from 'vuex'
+
+interface IRootState {
+    count: number
+}
+
+const store = createStore({
+    state (): IRootState {
+        return {
+            count: 0
+        }
+    },
+    mutations: {
+        increment (state: IRootState) {
+            state.count++
+        }
+    }
+})
 
 const app = createApp(App)
+
 elements.forEach(el => app.use(el))
+
 app.use(router)
+app.use(store)
 
 app.mount('#app')
