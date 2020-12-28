2195a255097b9198d7f84bd9f84566bdb3a323d9
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-28 17:15:40 +0300

    format

--- src/store/test/index.ts
+++ src/store/test/index.ts
@@ -1,6 +1,6 @@
 import { Module } from 'vuex'
 
-import { StoreState, state } from './state'
+import { state, StoreState } from './state'
 import { mutations } from './mutations'
 import { actions } from './actions'
 import { getters } from './getters'
