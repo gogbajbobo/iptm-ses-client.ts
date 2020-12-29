9f095251adf2cd9a43c77a55f4a6f7fe3615dbc2
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-29 10:28:00 +0300

    check isAuthorized before route processed

--- src/router/guards.ts
+++ src/router/guards.ts
@@ -1,10 +1,22 @@
-import { logger } from '@/services/logger'
 import { Router } from 'vue-router'
+import { store } from '@/store'
+import { GetterTypes } from '@/store/auth/types'
+import { paths } from './paths'
+
+const isAuthorized = !!store.getters[`auth/${ GetterTypes.USER }`]
 
 export const guardsInit = (router: Router) => {
 
     router.beforeEach((to, from) => {
+
+        if (!isAuthorized && !to.meta.anonymousAccess)
+            return paths.LOGIN
+
+        if (isAuthorized && to.path === paths.LOGIN)
+            return paths.ROOT
+
         return true
+
     })
 
     router.afterEach((to, from, failure) => {
