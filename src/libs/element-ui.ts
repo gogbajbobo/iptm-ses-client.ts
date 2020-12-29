8c6a109de49a36934f76f3b67f3aab7db33576f5
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-29 11:18:17 +0300

    added some elements

--- src/libs/element-ui.ts
+++ src/libs/element-ui.ts
@@ -1,6 +1,23 @@
-import { ElButton } from 'element-plus'  // full import — ~650Kib build
+import {
+
+    ElButton,
+    ElForm,
+    ElFormItem,
+    ElInput,
+
+    ElLoading,
+
+} from 'element-plus'  // full import — ~650Kib build
+
 import 'element-plus/lib/theme-chalk/index.css'
 
 export const elements = [
+
     ElButton,
+    ElForm,
+    ElFormItem,
+    ElInput,
+
+    ElLoading,
+
 ]
