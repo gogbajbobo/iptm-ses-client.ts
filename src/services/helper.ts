d9243ec4ac3a762a548d8771aa6b3ae42cf8fb4f
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-31 13:15:43 +0300

    Update helper.ts

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -1,1 +1,8 @@
+import { store } from '@/store'
+import * as authStore from '@/store/auth/types'
+
 export const isProduction = process.env.NODE_ENV === 'production'
+
+export const currentUser = () => store.getters[`auth/${ authStore.GetterTypes.USER }`]
+
+export const isAuthorized = () => !!currentUser()
