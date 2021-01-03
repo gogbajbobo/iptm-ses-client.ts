557e898250c22293735fcf754bf17026c9e064ca
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-03 19:00:37 +0300

    interceptors

--- src/services/network.ts
+++ src/services/network.ts
@@ -1,8 +1,27 @@
 import axios from 'axios'
 import { showError } from './messages'
+import { logger } from '@/services/logger'
 
 const axiosInstance = axios.create()
 axiosInstance.defaults.baseURL = 'http://localhost:8021'
+
+axiosInstance.interceptors.request.use(config => {
+
+    const { method, url } = config
+    logger.log(`send request ${ method }, ${ url }`)
+
+    return config
+
+}, Promise.reject)
+
+axiosInstance.interceptors.response.use(response => {
+
+    logger.log(`${ response.config.method } ${ response.config.url } response:`)
+    logger.log(response)
+
+    return response
+
+}, Promise.reject)
 
 export const login = (payload: object) =>
     axiosInstance.post('/login', payload)
