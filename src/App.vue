a5263755ebb00eecf181375f0aba7eb797cffb89
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-05 11:48:34 +0300

    Create TheLayout.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,27 @@
 <script lang="ts">
 
     import { defineComponent } from 'vue'
+    import TheLayout from '@/components/TheLayout.vue'
 
     export default defineComponent({
         name: 'App',
+        components: { TheLayout }
     })
 
 </script>
 
 <template>
-
-    <div>
-
-        <img alt="Vue logo" src="./assets/logo.png">
-        <router-view></router-view>
-
-    </div>
-
+    <TheLayout></TheLayout>
 </template>
 
 <style>
 
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
 </style>
---
+++ src/components/TheLayout.vue
@@ -0,0 +1,29 @@
+<script lang='ts'>
+
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+
+        name: 'TheLayout',
+
+    })
+
+</script>
+
+<template>
+
+    <div>
+        <img alt="Vue logo" src="../assets/logo.png" class='logo-image'>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<style scoped>
+
+    .logo-image {
+        margin-top: 24px;
+        margin-bottom: 24px;
+    }
+
+</style>
