2bb8b1303f240aec25e5c8cd003062a9e9e6199d
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-06 12:23:14 +0300

    getExamineeList

--- src/components/ExamineeList.vue
+++ src/components/ExamineeList.vue
@@ -1,6 +1,9 @@
 <script lang='ts'>
 
-    import { defineComponent } from 'vue'
+    import { defineComponent, computed } from 'vue'
+    import { useStore } from 'vuex'
+    import { ActionTypes, GetterTypes } from '@/store/examinees/types'
+
     const localname = 'Пользователи'
 
     export default defineComponent({
@@ -8,6 +11,19 @@
         name: 'ExamineeList',
         localname,
 
+        setup() {
+
+            const store = useStore()
+            const examineeList = computed(() => store.getters[`examinees/${ GetterTypes.EXAMINEE_LIST }`])
+            const getExamineeList = (() => store.dispatch(`examinees/${ ActionTypes.GET_EXAMINEES }`))()
+
+            return {
+                localname,
+                examineeList,
+                getExamineeList,
+            }
+
+        },
 
     })
 
