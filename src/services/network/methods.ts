a68a2cf39736e81c4d06895c815fd9d6706871d7
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-06 15:24:24 +0300

    refactor a bit

--- src/services/network/methods.ts
+++ src/services/network/methods.ts
@@ -1,12 +1,11 @@
 import axiosInstance from '@/services/network'
 import { rejectError } from '@/services/helper'
 
-export const get = (url: string) =>
-    axiosInstance.get(url)
+const request = (requestPromise: Promise<any>) =>
+    requestPromise
         .then(response => response.data)
         .catch(rejectError)
 
-export const post = (url: string, payload?: object) =>
-    axiosInstance.post(url, payload)
-        .then(response => response.data)
-        .catch(rejectError)
+export const get = (url: string) => request(axiosInstance.get(url))
+
+export const post = (url: string, payload?: object) => request(axiosInstance.post(url, payload))
