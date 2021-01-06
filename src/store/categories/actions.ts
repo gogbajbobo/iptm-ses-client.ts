4729596b64c5f7ee1c9c7c7b6f07054d78081355
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-06 15:15:49 +0300

    fix actions

--- src/store/categories/actions.ts
+++ src/store/categories/actions.ts
@@ -4,7 +4,7 @@ import { IRootState } from '@/store/interfaces'
 import { StoreState } from './state'
 import { ActionTypes, MutationTypes } from './types'
 
-import { getExaminees } from '@/services/network/examinees'
+import { getCategories } from '@/services/network/categories'
 
 export interface IActions {
     [ActionTypes.GET_CATEGORIES]: (context: ActionContext<StoreState, IRootState>) => Promise<any>
@@ -13,7 +13,7 @@ export interface IActions {
 export const actions: ActionTree <StoreState, IRootState> & IActions = {
 
     [ActionTypes.GET_CATEGORIES]: ({ commit }) => {
-        return getExaminees().then(data => commit(MutationTypes.SET_CATEGORY_LIST, data))
+        return getCategories().then(data => commit(MutationTypes.SET_CATEGORY_LIST, data))
     },
 
 }
