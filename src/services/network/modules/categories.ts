1b05ebe185a58e3ccd06c790dba0a6bc6e15047d
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-08 14:33:24 +0300

    deleteCategory

--- src/services/network/modules/categories.ts
+++ src/services/network/modules/categories.ts
@@ -1,8 +1,9 @@
 import { categoriesUrl } from '@/services/network/urls'
-import { get, post } from '@/services/network/methods'
+import { get, post, deleteRequest } from '@/services/network/methods'
 import { CategoryType } from '@/store/interfaces'
 
 type CategoryKey = 'category'
 
 export const getCategories = () => get(categoriesUrl)
 export const addCategory = (category: Record<CategoryKey, CategoryType>) => post(categoriesUrl, category)
+export const deleteCategory = (id: number) => deleteRequest(categoriesUrl, id)
