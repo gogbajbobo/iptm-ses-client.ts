07764934c036eb3075ff859695cd25b02f6b3c7b
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-08 11:36:34 +0300

    addCategory()

--- src/services/network/modules/categories.ts
+++ src/services/network/modules/categories.ts
@@ -1,4 +1,6 @@
-import { get } from '@/services/network/methods'
 import { categoriesUrl } from '@/services/network/urls'
+import { get, post } from '@/services/network/methods'
+import { CategoryType } from '@/store/interfaces'
 
 export const getCategories = () => get(categoriesUrl)
+export const addCategory = (category: CategoryType) => post(categoriesUrl, category)
