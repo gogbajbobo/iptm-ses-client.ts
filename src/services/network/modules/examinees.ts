1bb504ab86991e477dadc758a1ca4e8e57799cb9
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-09 14:04:01 +0300

    updateExaminee request

--- src/services/network/modules/examinees.ts
+++ src/services/network/modules/examinees.ts
@@ -1,4 +1,11 @@
-import { getRequest } from '@/services/network/methods'
 import { examineesUrl } from '@/services/network/urls'
+import { getRequest, putRequest } from '@/services/network/methods'
+import { UserType } from '@/store/interfaces'
+
+type ExamineeKey = 'examinee'
 
 export const getExaminees = () => getRequest(examineesUrl)
+
+export const updateExaminee = (id: number, examinee: Record<ExamineeKey, UserType>) => {
+    return putRequest(examineesUrl, id, examinee)
+}
