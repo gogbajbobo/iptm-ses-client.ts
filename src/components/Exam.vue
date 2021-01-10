d86e9e85022d864d2392a22233e1b44f1f2da746
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-10 14:55:25 +0300

    examItem component

---
+++ src/components/Exam.vue
@@ -0,0 +1,38 @@
+<script lang='ts'>
+
+import {defineComponent, PropType} from 'vue'
+import { ExamType } from '@/store/interfaces'
+
+    const localname = 'Экзамен'
+
+    export default defineComponent({
+
+        name: 'Exam',
+        localname,
+
+        props: {
+            examId: {
+                type: [ Number, String ],
+                required: true,
+            }
+        },
+
+        setup() {
+            return { localname }
+        },
+
+    })
+
+</script>
+
+<template>
+
+    <div>
+        <div>{{ localname }} {{ examId }}</div>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,13 @@ import Main from './Main.vue'
 import ExamineeList from '@/components/ExamineeList.vue'
 import CategoryList from '@/components/CategoryList.vue'
 import ExamList from '@/components/ExamList.vue'
+import Exam from '@/components/Exam.vue'
 
 export default {
     Login,
     Main,
     ExamineeList,
     CategoryList,
-    ExamList
+    ExamList,
+    Exam,
 }
