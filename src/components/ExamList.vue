cd0fbbc28766f8971493494f2c5024205b4ca861
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-10 12:26:23 +0300

    Create ExamList.vue

---
+++ src/components/ExamList.vue
@@ -0,0 +1,30 @@
+<script lang='ts'>
+
+    import { defineComponent } from 'vue'
+
+    const localname = 'Экзамены'
+
+    export default defineComponent({
+
+        name: 'ExamList',
+        localname,
+
+        setup() {
+            return { localname }
+        },
+
+    })
+
+</script>
+
+<template>
+
+    <div>
+        {{ localname }}
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,12 @@ import Login  from './Login.vue'
 import Main from './Main.vue'
 import ExamineeList from '@/components/ExamineeList.vue'
 import CategoryList from '@/components/CategoryList.vue'
+import ExamList from '@/components/ExamList.vue'
 
-export default { Login, Main, ExamineeList, CategoryList }
-
+export default {
+    Login,
+    Main,
+    ExamineeList,
+    CategoryList,
+    ExamList
+}
