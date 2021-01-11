ba14e8bc34d2ef24db7a0829f7186d2540238b88
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-11 09:41:55 +0300

    format / refactor a bit

--- src/services/network/modules/exams.ts
+++ src/services/network/modules/exams.ts
@@ -1,16 +1,16 @@
-import { getRequest, postRequest, putRequest, deleteRequest } from '@/services/network/methods'
+import { examsUrl as url } from '@/services/network/urls'
+import { ExamType as ItemType } from '@/store/interfaces'
 
-import { examsUrl } from '@/services/network/urls'
-import { ExamType } from '@/store/interfaces'
+import { getRequest, postRequest, putRequest, deleteRequest } from '@/services/network/methods'
 
 export type ItemKey = 'item'
 
-export const getItems = () => getRequest(examsUrl)
+export const getItems = () => getRequest(url)
 
-export const addItem = (item: Record<ItemKey, ExamType>) => postRequest(examsUrl, item)
+export const addItem = (item: Record<ItemKey, ItemType>) => postRequest(url, item)
 
-export const updateItem = (id: number, item: Record<ItemKey, ExamType>) => {
-    return putRequest(examsUrl, id, item)
+export const updateItem = (id: number, item: Record<ItemKey, ItemType>) => {
+    return putRequest(url, id, item)
 }
 
-export const deleteItem = (id: number) => deleteRequest(examsUrl, id)
+export const deleteItem = (id: number) => deleteRequest(url, id)
