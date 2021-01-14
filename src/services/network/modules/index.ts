8fe4b8aed36e9df8a2e016509ac91fe075aee35a
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-14 14:29:35 +0300

    Create network modules index / apiRequests<T>()

---
+++ src/services/network/modules/index.ts
@@ -0,0 +1,26 @@
+import { getRequest, postRequest, putRequest, deleteRequest } from '@/services/network/methods'
+
+type ItemKey = 'item'
+
+export type RequestCollection<T> = {
+    getItems: (params?: Record<string, string|number>) => Promise<T[]>
+    addItem: (item: Record<ItemKey, T>) => Promise<T>
+    updateItem: (id: number, item: Record<ItemKey, T>) => Promise<T>
+    deleteItem: (id: number) => Promise<string|number>
+}
+
+export function apiRequests<T>(url: string): RequestCollection<T> {
+
+    return {
+
+        getItems: (params?) => getRequest(url, params),
+
+        addItem: (item) => postRequest(url, item),
+
+        updateItem: (id, item) => putRequest(url, id, item),
+
+        deleteItem: (id) => deleteRequest(url, id),
+
+    }
+
+}
