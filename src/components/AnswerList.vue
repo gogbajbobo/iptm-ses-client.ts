2b14136e3e65535b3e5c15fe40ed661b5a51dfdf
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-03 15:45:35 +0300

    Create AnswerList.vue

---
+++ src/components/AnswerList.vue
@@ -0,0 +1,30 @@
+<script lang='ts'>
+
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+
+        name: 'AnswerList',
+
+        props: {
+            questionId: {
+                type: [ Number, String ],
+                required: true,
+            },
+        },
+
+    })
+
+</script>
+
+<template>
+
+    <div>
+        AnswerList {{ questionId }}
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
