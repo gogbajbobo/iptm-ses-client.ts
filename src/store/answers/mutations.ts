e467c30a1987534ca8edd42caad35ff6655ad06c
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-08 09:35:29 +0300

    handle multiple items in Mutations.REPLACE_ITEM

--- src/store/answers/mutations.ts
+++ src/store/answers/mutations.ts
@@ -1,3 +1,4 @@
+import { logger } from '@/services/logger'
 import { AnswerType } from '@/store/interfaces'
 import { initialState, StoreState } from './state'
 import { Mutations } from './types'
@@ -6,7 +7,7 @@ export type MutationsType<T> = {
     [Mutations.RESET]: (state: StoreState) => void
     [Mutations.SET_ITEMS]: (state: StoreState, itemList: T[] | null) => void
     [Mutations.ADD_ITEM]: (state: StoreState, item: T) => void
-    [Mutations.REPLACE_ITEM]: (state: StoreState, item: T) => void
+    [Mutations.REPLACE_ITEM]: (state: StoreState, item: T | T[]) => void
     [Mutations.DELETE_ITEM]: (state: StoreState, itemId: number) => void
 }
 
@@ -19,9 +20,34 @@ export const mutations: MutationsType<AnswerType> = {
     [Mutations.ADD_ITEM]: (state, item) => { state.itemList?.push(item) },
 
     [Mutations.REPLACE_ITEM]: (state, newItem) => {
-        state.itemList = state.itemList?.map(item => {
-            return item.id === newItem.id ? newItem : item
-        }) || null
+
+        if (Array.isArray(newItem)) {
+
+            const newItemIds = newItem.map(i => i.id)
+
+            state.itemList = state.itemList?.map(item => {
+
+                const newI = newItem.find(i => i.id === item.id)
+
+                if (!newItemIds.includes(item.id))
+                    return item
+
+                if (newI)
+                    return newI
+
+                logger.warn('something wrong with answers REPLACE_ITEM mutation')
+                return item
+
+            }) || null
+
+        } else {
+
+            state.itemList = state.itemList?.map(item => {
+                return item.id === newItem.id ? newItem : item
+            }) || null
+
+        }
+
     },
 
     [Mutations.DELETE_ITEM]: (state, itemId) => {
