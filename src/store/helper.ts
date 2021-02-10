a9b6bc8232ce8240964c6836e3cdf1c5be3a544c
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-10 15:12:40 +0300

    added questions

--- src/store/helper.ts
+++ src/store/helper.ts
@@ -1,10 +1,11 @@
-import { UserType, ExamType, CategoryType, SectionType, SectionEmbryo } from '@/store/interfaces'
 import { store } from '@/store/index'
 import * as authStore from '@/store/auth/types'
 import * as categoryStore from '@/store/categories/types'
 import * as sectionStore from '@/store/sections/types'
 import * as examStore from '@/store/exams/types'
+import * as questionStore from '@/store/questions/types'
 
+import { UserType, ExamType, CategoryType, SectionType, QuestionType, SectionEmbryo } from '@/store/interfaces'
 
 // AUTH
 export const currentUser = ():UserType|null => store.getters[`auth/${ authStore.GetterTypes.USER }`]
@@ -34,3 +35,10 @@ export const deleteSection = (sectionId: number):Promise<void> => {
 export const sections = ():SectionType[] => store.getters[`sections/${ sectionStore.Getters.ITEM_LIST }`]
 
 // QUESTIONS
+export const getQuestions = (params:Record<string, string|number>):Promise<void> => {
+    return store.dispatch(`questions/${ questionStore.Actions.GET_ITEMS }`, params)
+}
+export const deleteQuestion = (question: QuestionType):Promise<void> => {
+    return store.dispatch(`questions/${ questionStore.Actions.DELETE_ITEM }`, question.id)
+}
+export const questions = ():QuestionType[] => store.getters[`questions/${ questionStore.Getters.ITEM_LIST }`]
