c8c952b892209348bfaec161abd15c71b875270c
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-10 15:12:32 +0300

    added comments

--- src/store/helper.ts
+++ src/store/helper.ts
@@ -5,15 +5,20 @@ import * as categoryStore from '@/store/categories/types'
 import * as sectionStore from '@/store/sections/types'
 import * as examStore from '@/store/exams/types'
 
+
+// AUTH
 export const currentUser = ():UserType|null => store.getters[`auth/${ authStore.GetterTypes.USER }`]
 
+// CATEGORIES
 export const getCategories = ():Promise<void> => {
     return store.dispatch(`categories/${ categoryStore.ActionTypes.GET_CATEGORIES }`)
 }
 export const categories = ():CategoryType[] => store.getters[`categories/${ categoryStore.GetterTypes.CATEGORY_LIST }`]
 
+// EXAMS
 export const exams = ():ExamType[] => store.getters[`exams/${ examStore.Getters.ITEM_LIST }`]
 
+// SECTIONS
 export const getSections = (params:Record<string, string|number>):Promise<void> => {
     return store.dispatch(`sections/${ sectionStore.Actions.GET_ITEMS }`, params)
 }
@@ -27,3 +32,5 @@ export const deleteSection = (sectionId: number):Promise<void> => {
     return store.dispatch(`sections/${ sectionStore.Actions.DELETE_ITEM }`, sectionId)
 }
 export const sections = ():SectionType[] => store.getters[`sections/${ sectionStore.Getters.ITEM_LIST }`]
+
+// QUESTIONS
