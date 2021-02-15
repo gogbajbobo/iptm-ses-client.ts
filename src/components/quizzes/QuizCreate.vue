28c4465cc22faf0175f07b9db57b05ad3466d0f3
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-15 12:55:15 +0300

    rewrite QuizCreate
    to handle new workflow

--- src/components/quizzes/QuizCreate.vue
+++ src/components/quizzes/QuizCreate.vue
@@ -1,7 +1,7 @@
 <script lang='ts'>
 
     import { defineComponent, computed } from 'vue'
-    import { sections, getSections, categories, getExaminees, examinees, addQuiz } from '@/store/helper'
+    import { getExams, exams, getSections, sections, getExaminees, examinees } from '@/store/helper'
     import { paths } from '@/router/paths'
     import { sortBy } from 'lodash'
     import { QuizEmbryo, SectionType, UserType } from '@/store/interfaces'
@@ -15,36 +15,47 @@
         localname,
 
         props: {
-            categoryId: { type: [ Number, String ] },
+            examId: { type: [ Number, String ], required: true },
         },
 
         data() {
             return {
-                sectionSelection: [] as SectionType[],
                 examineeSelection: [] as UserType[],
             }
         },
 
         setup(props) {
 
-            const category = categories().find(c => c.id === Number(props.categoryId))
+            const exam = computed(() => computed(exams).value?.find(e => e.id === Number(props.examId)))
 
-            if (category) {
+            const categories = computed(() => computed(sections).value?.map(s => s.category))
+            const categoryIds = computed(() => computed(sections).value?.map(s => s.category.id))
 
-                const params = { category: category.id }
+            Promise.all([
+                getExams(),
+                getSections({ exam: props.examId })
+            ])
+                .then(() => getExaminees({ categories: categoryIds.value }))
+                .catch(() => {})
 
-                Promise.all([
-                    getSections(params),
-                    getExaminees(params)
-                ])
-                    .catch(() => {})
-
-            }
+            // const category = categories().find(c => c.id === Number(props.categoryId))
+            //
+            // if (category) {
+            //
+            //     const params = { category: category.id }
+            //
+            //     Promise.all([
+            //         getSections(params),
+            //         getExaminees(params)
+            //     ])
+            //         .catch(() => {})
+            //
+            // }
 
             return {
                 localname,
-                category,
-                sections: computed(() => sortBy(sections(), [ 'examId' ])),
+                exam,
+                categories,
                 examinees: computed(examinees),
             }
 
@@ -52,33 +63,33 @@
 
         methods: {
 
-            backToListButtonClicked() { this.$router.push(paths.QUIZ_LIST) },
-
-            sectionSelectionChange(selection: SectionType[]) {
-                this.sectionSelection = selection
-            },
-
-            examineeSelectionChange(selection: UserType[]) {
-                this.examineeSelection = selection
-            },
-
-            createQuiz() {
-
-                if (!this.examineeSelection.length || !this.sectionSelection.length)
-                    return showAlert('Не выбраны экзамены и/или пользователи!', 'Ошибка!')
-
-                const createQuizzes = this.sectionSelection.map(section => {
-                    return addQuiz({
-                        category: section.category.id,
-                        exam: section.examId,
-                        section: section.id,
-                        examinees: this.examineeSelection.map(e => e.id),
-                    })
-                })
-
-                Promise.all(createQuizzes).catch(() => {})
-
-            },
+            // backToListButtonClicked() { this.$router.push(paths.QUIZ_LIST) },
+            //
+            // sectionSelectionChange(selection: SectionType[]) {
+            //     this.sectionSelection = selection
+            // },
+            //
+            // examineeSelectionChange(selection: UserType[]) {
+            //     this.examineeSelection = selection
+            // },
+            //
+            // createQuiz() {
+            //
+            //     if (!this.examineeSelection.length || !this.sectionSelection.length)
+            //         return showAlert('Не выбраны экзамены и/или пользователи!', 'Ошибка!')
+            //
+            //     const createQuizzes = this.sectionSelection.map(section => {
+            //         return addQuiz({
+            //             category: section.category.id,
+            //             exam: section.examId,
+            //             section: section.id,
+            //             examinees: this.examineeSelection.map(e => e.id),
+            //         })
+            //     })
+            //
+            //     Promise.all(createQuizzes).catch(() => {})
+            //
+            // },
 
         },
 
@@ -92,53 +103,41 @@
 
         <div>{{ localname }}</div>
 
-        <template v-if='category'>
-            <div>Категория: {{ category.title }}</div>
-            <div>
-                <el-table :data='sections' @selection-change="sectionSelectionChange">
+        <template v-if='exam'>
 
-                    <el-table-column type="selection" width="55">
-                    </el-table-column>
-
-                    <el-table-column prop="exam.title" label='Экзамен'>
-                    </el-table-column>
-
-                    <el-table-column prop="title" label='Раздел'>
-                    </el-table-column>
-
-                </el-table>
-            </div>
-
+            <div>Экзамен: {{ exam.title }}</div>
+            <div>Категории:</div>
+            <div v-for='category in categories' :key='category.id'>{{ category.title }}</div>
             <el-divider></el-divider>
 
-            <div>
-                <el-table :data='examinees' @selection-change="examineeSelectionChange">
+<!--            <div>-->
+<!--                <el-table :data='examinees' @selection-change="examineeSelectionChange">-->
 
-                    <el-table-column type="selection" width="55">
-                    </el-table-column>
+<!--                    <el-table-column type="selection" width="55">-->
+<!--                    </el-table-column>-->
 
-                    <el-table-column prop="username" label='Имя пользователя'>
-                    </el-table-column>
+<!--                    <el-table-column prop="username" label='Имя пользователя'>-->
+<!--                    </el-table-column>-->
 
-                </el-table>
-            </div>
+<!--                </el-table>-->
+<!--            </div>-->
         </template>
 
         <template v-else>
-            <el-alert title='Неизвестная категория' type='error' center show-icon :closable='false'></el-alert>
+            <el-alert title='Неизвестный экзамен' type='error' center show-icon :closable='false'></el-alert>
         </template>
 
-        <div>
+<!--        <div>-->
 
-            <el-button type='primary'
-                       @click='createQuiz'
-                       class='create-quiz-button'>Создать тестирование</el-button>
+<!--            <el-button type='primary'-->
+<!--                       @click='createQuiz'-->
+<!--                       class='create-quiz-button'>Создать тестирование</el-button>-->
 
-        </div>
+<!--        </div>-->
 
-        <el-button type='text'
-                   @click='backToListButtonClicked'
-                   class='back-to-list-link'>К списку тестов</el-button>
+<!--        <el-button type='text'-->
+<!--                   @click='backToListButtonClicked'-->
+<!--                   class='back-to-list-link'>К списку тестов</el-button>-->
 
     </div>
 
