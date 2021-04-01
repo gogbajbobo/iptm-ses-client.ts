9e0748ddec38740bf1cb74de5c485575572020d8
gogbajbobo <grigoriev.maxim@gmail.com>
2021-04-01 16:37:40 +0300

    baseURL depends on mode

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -4,8 +4,10 @@ export enum UserRole {
     EXAMINEE = 'examinee',
 }
 
+export const isProduction = process.env.NODE_ENV === 'production'
+
 export const tokenPrefix = 'Bearer '
 
-export const baseURL = 'http://localhost:8021'
+export const baseURL = isProduction ? 'http://nano.iptm.ru:8000' : 'http://localhost:8021'
 
 export const NUMBER_OF_QUESTIONS = 10
