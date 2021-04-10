4946b63863617bc77f29efca10f011e3cf18311b
gogbajbobo <grigoriev.maxim@gmail.com>
2021-04-10 14:23:33 +0300

    edit baseURL

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -8,6 +8,6 @@ export const isProduction = process.env.NODE_ENV === 'production'
 
 export const tokenPrefix = 'Bearer '
 
-export const baseURL = isProduction ? 'http://nano.iptm.ru:8000' : 'http://localhost:8021'
+export const baseURL = isProduction ? 'http://ses.iptm.ru/api' : 'http://localhost:8021'
 
 export const NUMBER_OF_QUESTIONS = 10
