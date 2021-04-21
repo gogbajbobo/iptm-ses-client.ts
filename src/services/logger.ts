971ccaa5492f66872bd4ab15917baaaa75c2990f
gogbajbobo <grigoriev.maxim@gmail.com>
2021-04-21 12:31:54 +0300

    temporary output all logs in any mode

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,12 +1,20 @@
-import { isProduction } from '@/services/constants'
+// import { isProduction } from '@/services/constants'
 
 /* eslint-disable no-console */
+// export const logger = {
+//
+//     log: (...args: any) => isProduction || console.log(...args),
+//     info: (...args: any) => isProduction || console.info(...args),
+//     warn: (...args: any) => isProduction || console.warn(...args),
+//     error: (...args: any) => isProduction || console.error(...args)
+//
+// }
 export const logger = {
 
-    log: (...args: any) => isProduction || console.log(...args),
-    info: (...args: any) => isProduction || console.info(...args),
-    warn: (...args: any) => isProduction || console.warn(...args),
-    error: (...args: any) => isProduction ||  console.error(...args)
+    log: (...args: any) => console.log(...args),
+    info: (...args: any) => console.info(...args),
+    warn: (...args: any) => console.warn(...args),
+    error: (...args: any) => console.error(...args)
 
 }
 /* eslint-enable no-console */
